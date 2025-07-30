34d3d74c173af40164fafd4a87d4f601bbc93f69
nkululek0 <nkululekovuyo0103@gmail.com>
2025-07-30 13:31:44 +0200

    (Update): sets the main layout for the project

---
+++ src/App/App.jsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css'
+
+export function App() {
+
+  return (
+    <>
+      <section className={styles.main}>
+        <h1>App Component</h1>
+      </section>
+    </>
+  )
+}
